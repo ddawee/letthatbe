8d8fd7d9ddc9af28de18c338b8decc2f26d8357b
lee choong seob <67108727+ddawee@users.noreply.github.com>
2020-08-02 20:00:33 +0900

    Update and rename hl2kv.sh to callsignissue.sh

--- callsignissue.sh
+++ callsignissue.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-sudo -s♥
-cd /tmp♥
+sudo -s
+cd /tmp
 wget https://github.com/DVSwitch/MMDVM_Bridge/blob/master/dvswitch.sh♥
-chmod +x dvswitch.sh♥
+chmod +x dvswitch.sh
 cp ./dvswitch.sh /opt/Analog_Bridge
 /opt/Analog_Bridge/dvswitch.sh update
 #shutdown -r now
