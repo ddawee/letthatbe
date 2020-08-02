4c8932f8c75cbcc74b60a337259d8466bcc2aa7c
lee choong seob <67108727+ddawee@users.noreply.github.com>
2020-08-02 20:01:39 +0900

    Update callsignissue.sh

--- callsignissue.sh
+++ callsignissue.sh
@@ -2,7 +2,7 @@
 
 sudo -s
 cd /tmp
-wget https://github.com/DVSwitch/MMDVM_Bridge/blob/master/dvswitch.sh♥
+wget https://github.com/DVSwitch/MMDVM_Bridge/blob/master/dvswitch.sh
 chmod +x dvswitch.sh
 cp ./dvswitch.sh /opt/Analog_Bridge
 /opt/Analog_Bridge/dvswitch.sh update
